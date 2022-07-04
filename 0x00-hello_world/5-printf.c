6f0aa0458151d81940bd63accc029084f30d70c7
u-tugi <mututawanja@gmail.com>
2022-07-04 09:59:53 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /
- * main - Prints "with proper grammar, but the outcome is a piece of art,"
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Return: Always 0 (success)
  */
 int main(void)
 {
